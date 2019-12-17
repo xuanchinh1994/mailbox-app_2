82b8709695447c37f32491109acadc4903c80d2a
xuanchinh1994 <xuanchinh1994@gmail.com>
2019-12-18 00:28:53 +0700

    Initial commit

---
+++ app/src/main/java/com/cognition/android/mailboxapp/activities/EmailActivity.java
@@ -0,0 +1,396 @@
+package com.cognition.android.mailboxapp.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.cognition.android.mailboxapp.R;
+import com.cognition.android.mailboxapp.models.Message;
+import com.cognition.android.mailboxapp.models.Message_Table;
+import com.cognition.android.mailboxapp.utils.Utils;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.ExponentialBackOff;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.transitionseverywhere.TransitionManager;
+import com.wang.avi.AVLoadingIndicatorView;
+
+import com.google.api.services.gmail.Gmail;
+//import com.google.api.services.gmail.model.Message;
+import com.google.api.services.gmail.model.MessagePart;
+import com.google.api.services.gmail.model.MessagePartBody;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import static com.cognition.android.mailboxapp.activities.MainActivity.PREF_ACCOUNT_NAME;
+import static com.cognition.android.mailboxapp.activities.MainActivity.SCOPES;
+
+public class EmailActivity extends AppCompatActivity {
+
+    CoordinatorLayout lytParent;
+    Toolbar toolbar;
+    AppCompatTextView txtSubject, txtFromPreview, txtFrom, txtDate;
+    ConstraintLayout lytFromPreviewParent;
+    WebView myWebView;
+    AVLoadingIndicatorView avLoadingIndicatorView;
+    LinearLayoutCompat lytError;
+    AppCompatButton btnRetry;
+    GoogleAccountCredential mCredential;
+    Gmail mService;
+    SharedPreferences sharedPref;
+
+    Utils mUtils;
+    Message mMessage = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+
+        mService = null;
+
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
+        sharedPref = EmailActivity.this.getSharedPreferences(getString(R.string.preferences_file_name), Context.MODE_PRIVATE);
+        mUtils = new Utils(EmailActivity.this);
+
+        String accountName = sharedPref.getString(PREF_ACCOUNT_NAME, null);
+        if (accountName != null) {
+
+                    mCredential.setSelectedAccountName(accountName);
+
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            mService = new com.google.api.services.gmail.Gmail.Builder(
+                    transport, jsonFactory, mCredential)
+                .setApplicationName("MailBox App")
+                .build();
+        }
+
+
+        int messageId = getIntent().getIntExtra("messageId", -1);
+        String messageGmailId = getIntent().getStringExtra("messageGmailId");
+
+        mMessage = SQLite.select().from(Message.class).where(Message_Table.id.eq(messageId)).querySingle();
+
+        try {
+            getAttachments(mService, "me", messageGmailId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (mMessage != null) {
+            initViews();
+            setUpWebView();
+        } else
+            onBackPressed();
+    }
+
+    /**
+     * Initialize views
+     */
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initViews() {
+        lytParent = findViewById(R.id.lytParent);
+        toolbar = findViewById(R.id.toolbar);
+        txtSubject = findViewById(R.id.txtSubject);
+        txtFromPreview = findViewById(R.id.txtFromPreview);
+        txtFrom = findViewById(R.id.txtFrom);
+        txtDate = findViewById(R.id.txtDate);
+        lytFromPreviewParent = findViewById(R.id.lytFromPreviewParent);
+        myWebView = findViewById(R.id.webView);
+        avLoadingIndicatorView = findViewById(R.id.aviLoadingIndicator);
+        lytError = findViewById(R.id.lytError);
+        btnRetry = findViewById(R.id.btnRetry);
+
+        toolbar.setTitle(mMessage.getSubject());
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        txtSubject.setText(mMessage.getSubject());
+        txtFromPreview.setText(mMessage.getFrom().substring(0, 1).toUpperCase(Locale.ENGLISH));
+        txtFrom.setText(mMessage.getFrom());
+        txtDate.setText(mUtils.timestampToDate(mMessage.getTimestamp()));
+
+        android.graphics.drawable.GradientDrawable gradientDrawable = (android.graphics.drawable.GradientDrawable) lytFromPreviewParent.getBackground();
+        gradientDrawable.setColor(mMessage.getColor());
+
+        WebSettings myWebSettings = myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebSettings.setUseWideViewPort(true);
+        myWebSettings.setSupportZoom(true);
+        myWebSettings.setBuiltInZoomControls(true);
+        myWebSettings.setDisplayZoomControls(false);
+        myWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        myWebSettings.setDomStorageEnabled(true);
+        myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        myWebView.setScrollbarFadingEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                TransitionManager.beginDelayedTransition(lytParent);
+                lytError.setVisibility(View.GONE);
+                avLoadingIndicatorView.smoothToHide();
+                myWebView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int statuscode, String description, String url) {
+                super.onReceivedError(view, statuscode, description, url);
+
+                Log.e(MainActivity.TAG, String.format("code: %d\tdescription: %s\turl: %s", statuscode, description, url));
+
+                TransitionManager.beginDelayedTransition(lytParent);
+                myWebView.setVisibility(View.GONE);
+                lytError.setVisibility(View.VISIBLE);
+                avLoadingIndicatorView.smoothToHide();
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+
+                Log.e(MainActivity.TAG, String.format("code: %d\tdescription: %s\turl: %s", error.getErrorCode(), error.getDescription(), request.getUrl()));
+
+                TransitionManager.beginDelayedTransition(lytParent);
+                myWebView.setVisibility(View.GONE);
+                lytError.setVisibility(View.VISIBLE);
+                avLoadingIndicatorView.smoothToHide();
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse response) {
+                super.onReceivedHttpError(view, request, response);
+
+                Log.e(MainActivity.TAG, String.format("code: %d\tdescription: %s\turl: %s", response.getStatusCode(), response.getReasonPhrase(), request.getUrl()));
+
+                TransitionManager.beginDelayedTransition(lytParent);
+                myWebView.setVisibility(View.GONE);
+                lytError.setVisibility(View.VISIBLE);
+                avLoadingIndicatorView.smoothToHide();
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                super.onReceivedSslError(view, handler, error);
+                handler.proceed();
+
+                Log.e(MainActivity.TAG, error.toString());
+
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                return true;
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(request.getUrl());
+                startActivity(i);
+                return true;
+            }
+
+        });
+
+        btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUpWebView();
+            }
+        });
+    }
+
+    /**
+     * Prepare the webview to display the email
+     */
+    private void setUpWebView() {
+        TransitionManager.beginDelayedTransition(lytParent);
+        myWebView.setVisibility(View.GONE);
+        lytError.setVisibility(View.GONE);
+        avLoadingIndicatorView.smoothToShow();
+
+        try {
+            JSONObject parentPart = new JSONObject(mMessage.getParentPartJson());
+
+            if (parentPart.getJSONObject("body").getInt("size") != 0) {
+                byte[] dataBytes = Base64.decodeBase64(parentPart.getJSONObject("body").getString("data"));
+                String data = new String(dataBytes, "UTF-8");
+
+                myWebView.loadData(data, mMessage.getMimetype(), "UTF-8");
+                Log.d(MainActivity.TAG, mMessage.getMimetype());
+            } else {
+                JSONArray partsArray = new JSONArray(mMessage.getPartsJson());
+
+                String[] result = getData(partsArray);
+                if (result[0] != null && result[1] != null) {
+                    myWebView.loadData(result[1], result[0], "UTF-8");
+                    Log.d(MainActivity.TAG, result[0]);
+                } else {
+                    TransitionManager.beginDelayedTransition(lytParent);
+                    myWebView.setVisibility(View.GONE);
+                    lytError.setVisibility(View.VISIBLE);
+                    avLoadingIndicatorView.smoothToHide();
+                }
+            }
+        } catch (Exception e) {
+            Log.w(MainActivity.TAG, e);
+            e.printStackTrace();
+
+            TransitionManager.beginDelayedTransition(lytParent);
+            myWebView.setVisibility(View.GONE);
+            lytError.setVisibility(View.VISIBLE);
+            avLoadingIndicatorView.smoothToHide();
+        }
+    }
+
+    /**
+     * Get email data
+     *
+     * @param parts JSONArray
+     * @return String[]
+     * @throws JSONException                JSONException
+     * @throws UnsupportedEncodingException UnsupportedEncodingException
+     */
+    private String[] getData(JSONArray parts) throws JSONException, UnsupportedEncodingException {
+        for (int i = 0; i < parts.length(); i++) {
+            JSONObject part = new JSONObject(parts.getString(i));
+            if (part.has("parts"))
+                return getData(new JSONArray(part.getString("parts")));
+            else {
+                if (part.getString("mimeType").equals("text/html"))
+                    return new String[]{
+                            part.getString("mimeType"),
+                            new String(
+                                    Base64.decodeBase64(part.getJSONObject("body").getString("data")),
+                                    "UTF-8"
+                            )
+                    };
+            }
+        }
+
+        for (int i = 0; i < parts.length(); i++) {
+            JSONObject part = new JSONObject(parts.getString(i));
+            if (part.getString("mimeType").equals("text/plain"))
+                return new String[]{
+                        part.getString("mimeType"),
+                        new String(
+                                Base64.decodeBase64(part.getJSONObject("body").getString("data")),
+                                "UTF-8"
+                        )
+                };
+        }
+
+        return new String[]{null, null};
+    }
+
+    /**
+     * Get email data
+     *
+     * @param parts JSONArray
+     * @return String[]
+     * @throws JSONException                JSONException
+     * @throws UnsupportedEncodingException UnsupportedEncodingException
+     */
+    private String[] getDataAttachment(JSONArray parts) throws JSONException, UnsupportedEncodingException {
+        for (int i = 0; i < parts.length(); i++) {
+            JSONObject part = new JSONObject(parts.getString(i));
+            if (part.has("partId"))
+                return getData(new JSONArray(part.getString("parts")));
+            else {
+                if (part.getString("mimeType").equals("application/octet-stream"))
+                    return new String[]{
+                            part.getString("mimeType"),
+                            new String(
+                                    Base64.decodeBase64(part.getJSONObject("body").getString("data")),
+                                    "UTF-8"
+                            )
+                    };
+            }
+        }
+
+        for (int i = 0; i < parts.length(); i++) {
+            JSONObject part = new JSONObject(parts.getString(i));
+            if (part.getString("mimeType").equals("text/plain"))
+                return new String[]{
+                        part.getString("mimeType"),
+                        new String(
+                                Base64.decodeBase64(part.getJSONObject("body").getString("data")),
+                                "UTF-8"
+                        )
+                };
+        }
+
+        return new String[]{null, null};
+    }
+
+    public static void getAttachments(Gmail service, String userId, String messageId)
+            throws IOException {
+        com.google.api.services.gmail.model.Message message = service.users().messages().get(userId, messageId).execute();
+        List<MessagePart> parts = message.getPayload().getParts();
+        for (MessagePart part : parts) {
+            if (part.getFilename() != null && part.getFilename().length() > 0) {
+                String filename = part.getFilename();
+                String attId = part.getBody().getAttachmentId();
+                MessagePartBody attachPart = service.users().messages().attachments().
+                        get(userId, messageId, attId).execute();
+
+//                Base64 base64Url = new Base64(true);
+//                byte[] fileByteArray = base64url.decodeBase64(attachPart.getData());
+//                FileOutputStream fileOutFile =
+//                        new FileOutputStream("directory_to_store_attachments" + filename);
+//                fileOutFile.write(fileByteArray);
+//                fileOutFile.close();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/cognition/android/mailboxapp/models/Message.java
@@ -0,0 +1,230 @@
+package com.cognition.android.mailboxapp.models;
+
+import com.google.api.services.gmail.model.MessagePart;
+import com.raizlabs.android.dbflow.annotation.ColumnIgnore;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Map;
+
+@Table(database = AppDatabase.class, allFields = true)
+public class Message extends BaseModel {
+
+    @PrimaryKey(autoincrement = true)
+    private int id;
+    private int message_id;
+    private String message_gmail_id;
+    private String attachmentId;
+    private String labelsJson;
+    private String snippet;
+    private String mimetype;
+    private String headersJson;
+    private String parentPartJson;
+    private String partsJson;
+    private String from;
+    private String subject;
+    private long timestamp;
+    private int color;
+
+    @ColumnIgnore
+    private List<String> labels;
+    @ColumnIgnore
+    private Map<String, String> headers;
+    @ColumnIgnore
+    private MessagePart parentPart;
+    @ColumnIgnore
+    private List<MessagePart> parts;
+
+    public Message() {
+    }
+
+    public Message(String message_gmail_id,String attachmentId,List<String> labels, String snippet, String mimetype, Map<String, String> headers, List<MessagePart> parts, long timestamp, int color, MessagePart parentPart) throws JSONException {
+        this.message_gmail_id = message_gmail_id;
+        this.attachmentId = attachmentId;
+        this.labels = labels;
+        this.snippet = snippet;
+        this.mimetype = mimetype;
+        this.headers = headers;
+        this.parts = parts;
+        this.timestamp = timestamp;
+        this.parentPart = parentPart;
+
+        this.from = this.headers.get("From");
+        this.subject = this.headers.get("Subject");
+        this.color = color;
+
+        if (this.labels != null) {
+            JSONArray labelsArr = new JSONArray();
+            for (String label : this.labels)
+                labelsArr.put(label);
+            this.labelsJson = labelsArr.toString();
+        }
+
+        if (this.headers != null) {
+            JSONObject headersObj = new JSONObject();
+            for (Map.Entry<String, String> header : this.headers.entrySet())
+                headersObj.put(header.getKey(), header.getValue());
+            this.headersJson = headersObj.toString();
+        }
+
+        if (this.parts != null) {
+            JSONArray partsArr = new JSONArray();
+            for (MessagePart messagePart : this.parts)
+                partsArr.put(messagePart.toString());
+            this.partsJson = partsArr.toString();
+        }
+
+        if (this.parentPart != null)
+            this.parentPartJson = parentPart.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMessage_id() {
+        return message_id;
+    }
+
+    public void setMessage_id(int message_id) {
+        this.message_id = message_id;
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<String> labels) {
+        this.labels = labels;
+    }
+
+    public String getSnippet() {
+        return snippet;
+    }
+
+    public void setSnippet(String snippet) {
+        this.snippet = snippet;
+    }
+
+    public String getMimetype() {
+        return mimetype;
+    }
+
+    public void setMimetype(String mimetype) {
+        this.mimetype = mimetype;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public List<MessagePart> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<MessagePart> parts) {
+        this.parts = parts;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public String getLabelsJson() {
+        return labelsJson;
+    }
+
+    public void setLabelsJson(String labelsJson) {
+        this.labelsJson = labelsJson;
+    }
+
+    public String getHeadersJson() {
+        return headersJson;
+    }
+
+    public void setHeadersJson(String headersJson) {
+        this.headersJson = headersJson;
+    }
+
+    public String getPartsJson() {
+        return partsJson;
+    }
+
+    public void setPartsJson(String partsJson) {
+        this.partsJson = partsJson;
+    }
+
+    public MessagePart getParentPart() {
+        return parentPart;
+    }
+
+    public void setParentPart(MessagePart parentPart) {
+        this.parentPart = parentPart;
+    }
+
+    public String getParentPartJson() {
+        return parentPartJson;
+    }
+
+    public void setParentPartJson(String parentPartJson) {
+        this.parentPartJson = parentPartJson;
+    }
+
+    public String getMessage_gmail_id() {
+        return message_gmail_id;
+    }
+
+    public void setMessage_gmail_id(String message_gmail_id) {
+        this.message_gmail_id = message_gmail_id;
+    }
+
+    public String getAttachmentId() {
+        return attachmentId;
+    }
+
+    public void setAttachmentId(String attachmentId) {
+        this.attachmentId = attachmentId;
+    }
+}
